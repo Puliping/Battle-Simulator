204c353305ae3063b536ae51a905d26e515209ac
Paulo Rubens <paulo.rubens@usp.br>
2022-11-26 19:24:51 -0300

    wip troop spawner

---
+++ Assets/Scripts/TroopSpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TroopSpawner : MonoBehaviour
+{
+    enum Team
+    {
+        Blue,
+        Red
+    }
+    public void SpawnTroop(bool red)
+    {
+        // TODO do stuff
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
