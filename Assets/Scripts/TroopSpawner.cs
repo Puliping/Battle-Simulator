b301abc0f4dd9d62c0bc53c2e198c988738eb83d
Paulo Rubens <paulo.rubens@usp.br>
2022-11-30 15:50:19 -0300

    fix troop spawner

--- Assets/Scripts/TroopSpawner.cs
+++ Assets/Scripts/TroopSpawner.cs
@@ -9,19 +9,35 @@ public class TroopSpawner : MonoBehaviour
         Blue,
         Red
     }
+
+    public Collider blueSpawn;
+    public GameObject blueInfantry;
+    // public GameObject blueCavalry;
+    // public GameObject blueArcher;
+    public Collider redSpawn;
+    public GameObject redInfantry;
+    // public GameObject redCavalry;
+    // public GameObject redArcher;
     public void SpawnTroop(bool red)
     {
-        // TODO do stuff
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        GameObject toSpawn;
+        Vector3 min, max;
+        if (red) {
+            toSpawn = redInfantry;
+            min = redSpawn.bounds.min;
+            max = redSpawn.bounds.max;
+        } else {
+            toSpawn = blueInfantry;
+            min = blueSpawn.bounds.min;
+            max = blueSpawn.bounds.max;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 pointToSpawn = new Vector3(
+            Random.Range(min.x, max.x),
+            1.5f,
+            Random.Range(min.z, max.z)
+        );
+
+        GameObject.Instantiate(toSpawn, pointToSpawn, toSpawn.transform.rotation);
     }
 }
