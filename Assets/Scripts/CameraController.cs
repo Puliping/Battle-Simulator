ceebd9ae688a684ba72391a6ee8ec86115aaf08b
Paulo Rubens <paulo.rubens@usp.br>
2022-12-14 03:12:37 -0300

    new camera controls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,168 +1,80 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour
 {
-    private Controls cameraActions;
-    private InputAction movement;
-    private Transform cameraTransform;
+    public float speed = 25; // how fast the camera moves
+    public float acceleration = 50; // how fast the camera accelerates to top speed
+    public float accSprintMultiplier = 4; // how much faster the camera goes when sprinting
+    public float lookSensitivity = 1; // mouse look sensitivity
+    public float dampingCoefficient = 10; // how much time it takes for the camera to break
+    public bool focusOnEnable = false; // whether or not to focus and lock cursor immediately on enable
 
-    public float maxSpeed = 5f;
-    private float speed;
-    public float acceleration = 10f;
-    public float damping = 10f;
+    Vector3 velocity; // current velocity
 
-    public float stepSize = 2f;
-    public float zoomDamping = 7.5f;
-    public float minHeight = 5f;
-    public float maxHeight = 50f;
-    public float zoomSpeed = 2f;
-
-    public float maxRotationSpeed = 1f;
-
-    private Vector3 targetPosition;
-
-    private float zoomHeight;
-
-    private Vector3 horizontalVelocity;
-    private Vector3 lastPosition;
-
-    private Vector3 startDrag;
-
-    private void Awake()
+    static bool Focused
     {
-        cameraActions = new Controls();
-        cameraTransform = this.GetComponentInChildren<Camera>().transform;
-    }
-
-    private void OnEnable()
-    {
-        zoomHeight = cameraTransform.localPosition.y;
-        cameraTransform.LookAt(this.transform);
-
-        lastPosition = this.transform.position;
-
-        movement = cameraActions.Camera.Move;
-        cameraActions.Camera.Rotate.performed += Rotate;
-        cameraActions.Camera.Zoom.performed += Zoom;
-        cameraActions.Camera.Enable();
-    }
-
-    private void OnDisable()
-    {
-        cameraActions.Camera.Rotate.performed -= Rotate;
-        cameraActions.Camera.Zoom.performed -= Zoom;
-        cameraActions.Camera.Disable();
-    }
-
-    private void Zoom(InputAction.CallbackContext context)
-    {
-        float inputValue = -context.ReadValue<Vector2>().y / 100f;
-
-        if (Mathf.Abs(inputValue) > 0.1f)
+        get => Cursor.lockState == CursorLockMode.Locked;
+        set
         {
-            zoomHeight = cameraTransform.localPosition.y + inputValue * stepSize;
-
-            if (zoomHeight < minHeight)
-                zoomHeight = minHeight;
-            else if (zoomHeight > maxHeight)
-                zoomHeight = maxHeight;
+            Cursor.lockState = value ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = value == false;
         }
     }
 
-    private void Rotate(InputAction.CallbackContext context)
-    {
-        if (!Mouse.current.rightButton.isPressed)
-            return;
+    void OnEnable() => Focused = focusOnEnable;
 
-        float inputValue = context.ReadValue<Vector2>().x;
-        float rotY = inputValue * maxRotationSpeed + transform.rotation.eulerAngles.y;
-        transform.rotation = Quaternion.Euler(0f, rotY, 0f);
+    void OnDisable() => Focused = false;
+
+    void Update()
+    {
+        // Position
+        velocity += speed * GetAccelerationVector() * Time.deltaTime;
+
+        // Rotation
+        if (Focused)
+            UpdateInput();
+        else if (Input.GetMouseButtonDown(1))
+            Focused = true;
+
+        // Physics
+        velocity = Vector3.Lerp(velocity, Vector3.zero, dampingCoefficient * Time.deltaTime);
+        transform.position += velocity * Time.deltaTime;
     }
 
-    private void Update()
+    void UpdateInput()
     {
-        //inputs
-        GetKeyboardMovement();
-        DragCamera();
+        Vector2 mouseDelta = lookSensitivity * new Vector2(Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"));
+        Quaternion rotation = transform.rotation;
+        Quaternion horiz = Quaternion.AngleAxis(mouseDelta.x, Vector3.up);
+        Quaternion vert = Quaternion.AngleAxis(mouseDelta.y, Vector3.right);
+        transform.rotation = horiz * rotation * vert;
 
-        //move base and camera objects
-        UpdateVelocity();
-        UpdateRigPosition();
-        UpdateCameraPosition();
+        // Leave cursor lock
+        if (Input.GetMouseButtonUp(1))
+            Focused = false;
     }
 
-    private void GetKeyboardMovement()
+    Vector3 GetAccelerationVector()
     {
-        Vector3 cameraForward = cameraTransform.forward;
-        cameraForward.y = 0f;
-        Vector3 cameraRight = cameraTransform.right;
-        cameraRight.y = 0f;
+        Vector3 moveInput = default;
 
-        Vector3 inputValue = (movement.ReadValue<Vector2>().x * cameraRight
-                            + movement.ReadValue<Vector2>().y * cameraForward)
-                            .normalized;
-
-        if (inputValue.sqrMagnitude > 0.1f)
-            targetPosition += inputValue;
-    }
-
-    private void DragCamera()
-    {
-        if (!Mouse.current.middleButton.isPressed)
-            return;
-
-        //create plane to raycast to
-        Plane plane = new Plane(Vector3.up, Vector3.zero);
-        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-    
-        if(plane.Raycast(ray, out float distance))
+        void AddMovement(KeyCode key, Vector3 dir)
         {
-            if (Mouse.current.middleButton.wasPressedThisFrame)
-                startDrag = ray.GetPoint(distance);
-            else
-                targetPosition += startDrag - ray.GetPoint(distance);
-        }
-    }
-
-    private void UpdateVelocity()
-    {
-        horizontalVelocity = (this.transform.position - lastPosition) / Time.deltaTime;
-        horizontalVelocity.y = 0f;
-        lastPosition = this.transform.position;
-    }
-
-    private void UpdateRigPosition()
-    {
-        if (targetPosition.sqrMagnitude > 0.1f)
-        {
-            //create a ramp up or acceleration
-            speed = Mathf.Lerp(speed, maxSpeed, Time.deltaTime * acceleration);
-            transform.position += targetPosition * speed * Time.deltaTime;
-        }
-        else
-        {
-            //create smooth slow down
-            horizontalVelocity = Vector3.Lerp(horizontalVelocity, Vector3.zero, Time.deltaTime * damping);
-            transform.position += horizontalVelocity * Time.deltaTime;
+            if (Input.GetKey(key))
+                moveInput += dir;
         }
 
-        //reset for next frame
-        targetPosition = Vector3.zero;
-    }
+        AddMovement(KeyCode.W, Vector3.forward);
+        AddMovement(KeyCode.S, Vector3.back);
+        AddMovement(KeyCode.D, Vector3.right);
+        AddMovement(KeyCode.A, Vector3.left);
+        AddMovement(KeyCode.Q, Vector3.up);
+        AddMovement(KeyCode.E, Vector3.down);
+        Vector3 direction = transform.TransformVector(moveInput.normalized);
 
-    private void UpdateCameraPosition()
-    {
-        //set zoom target
-        Vector3 zoomTarget = new Vector3(cameraTransform.localPosition.x, zoomHeight, cameraTransform.localPosition.z);
-        //add vector for forward/backward zoom
-        zoomTarget -= zoomSpeed * (zoomHeight - cameraTransform.localPosition.y) * Vector3.forward;
-
-        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, zoomTarget, Time.deltaTime * zoomDamping);
-        cameraTransform.LookAt(this.transform);
-
+        if (Input.GetKey(KeyCode.LeftShift))
+            return direction * (acceleration * accSprintMultiplier); // "sprinting"
+        return direction * acceleration; // "walking"
     }
 }
